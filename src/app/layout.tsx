840fa2c1216417f244327778501c72ef831a87cd
ridethehillcountry <ridethehillcountry@gmail.com>
2023-08-09 08:03:08 -0500

    added some style

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image"
 import Link from "next/link"
 import "./globals.css"
 
@@ -10,17 +9,13 @@ export default function RootLayout({
   const header = (
     <header>
       <div className='text-center bg-slate-800 p-8 my-6 rounded-md'>
-        <Image
-          src='/logo.png'
-          width={40}
-          height={40}
-          className='mx-auto'
-          alt={"logo"}
-        />
+       
         <Link href='/'>
-          <h1 className='text-2xl text-white font-bold mt-4'>Jeff's Blog</h1>
+          <h1 className='text-2xl text-white font-bold mt-4'>
+            Ride the Hill Country
+          </h1>
         </Link>
-        <p className='text-slate-300'>Welcome to Ride the Hill Country.</p>
+        <p className='text-slate-300'>Welcome</p>
       </div>
     </header>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,13 @@ export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-2'>
       <div>
+        <Image
+          src='/cover.jpeg'
+          width={1280}
+          height={240}
+          className='mx-auto'
+          alt={"logo"}
+        />
         <h1>Hello World</h1>
         <p className=''>This is a really good blog</p>
       </div>
