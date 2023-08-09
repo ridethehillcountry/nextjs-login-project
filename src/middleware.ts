3208090b11e0af87408044270e785f9f4166073c
ridethehillcountry <ridethehillcountry@gmail.com>
2023-08-09 07:10:18 -0500

    added email verify

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,7 +5,7 @@ export function middleware(request: NextRequest) {
   const path = request.nextUrl.pathname
 
   const isPublicPath =
-    path === "/login" || path === "/signup" 
+    path === "/login" || path === "/signup" || path === "/verifyemail"
 
   const token = request.cookies.get("token")?.value || ""
 
@@ -20,5 +20,5 @@ export function middleware(request: NextRequest) {
 
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: ["/", "/profile", "/login", "/signup"],
+  matcher: ["/", "/profile", "/login", "/signup", "/verifyemail"],
 }
